5aa5b649d20dc4215112dab01f83607783589edc
mosheurpau <mosheur.pau@gamil.com>
2023-12-06 02:24:03 +0600

    State, component state hook and set state method

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <dev>
+      <Counter></Counter>
+    </dev>
+  );
+
+  /* 
+  const items = [
+    { name: "Laptop1", price: "85k" },
+    { name: "Laptop2", price: "75k" },
+    { name: "Laptop3", price: "65k" },
+    { name: "Laptop4", price: "55k" },
+  ];
+  return (
+   <div className="App">
+      {items.map((item) => (
+        <Products name={item.name} price={item.price}></Products>
+      ))}
+    </div>
+  );
+ */
+}
+
+/*
+function Products(props) {
+
+  console.log(props);
+  return (
+    <div
+      style={{
+        background: "blue",
+        padding: "10px",
+        margin: "20px",
+        border: "solid red 5px",
+        borderRadius: "15px",
+      }}
+    >
+      <h2>Neme: {props.name}</h2>
+      <h5>Price: {props.price}</h5>
+    </div>
+  );
+ */
+
+function Counter() {
+  const [count, setCount] = useState(5);
+  console.log(count);
+
+  const increaseCount = () => setCount(count + 1);
+  const decreaseCount = () => setCount(count - 1);
+
+  // const decreaseCount = () => {
+  //   const newCount = count - 1;
+  //   setCount(newCount);
+  // }
+
+  return (
+    <div>
+      <h2>Number: {count}</h2>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
     </div>
   );
 }
